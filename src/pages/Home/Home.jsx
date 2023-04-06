b2d589add735621adeccadcc335e5e00652fb5c8
Luis Guedes <luisguedes.dev@gmail.com>
2023-04-06 01:57:09 -0300

    implementação da page livros menos emprestados

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,67 @@
+import { useEffect, useState } from "react";
+import { Button, Carousel, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { getEmprestimos } from "../../firebase/emprestimos";
+import { getLivros } from "../../firebase/livros";
+import { Loader } from "../../components/Loader/Loader";
+import "../Home/home.css"
+
 export function Home() {
-  return <div className="text-center">
-    <br />
-    <br />
-    HOME
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-  </div>;
+  const [livros, setLivros] = useState([]);
+  const [emprestimos, setEmprestimos] = useState(null);
+
+  function situacaoLivro (status, entrega) {
+    if (status === "Devolvido") {
+      return "Devolvido";
+    } else {
+      const dateNow = new Date();
+      if (dateNow > entrega) {
+        return "Atrasado";
+      } else {
+        return "Pendente";
+      }
+    }
+  }
+
+  useEffect(() => {
+    getLivros().then((livros) => {
+      setLivros(livros.map((livro) => ({ ...livro, emprestimos: 0 })));
+    });
+    getEmprestimos().then((emprestimos) => {
+      emprestimos.forEach((emprestimo) => {
+        setLivros((livros) => {
+          const novoLivros = [...livros];
+          const index = novoLivros.findIndex(
+            (livro) => livro.id === emprestimo.livro.id
+          );
+          novoLivros[index].emprestimos++;
+          return novoLivros;
+        });
+      });
+      setEmprestimos(emprestimos);
+    });
+  }, []);
+
+  const livrosOrdenados = livros.sort((a, b) => a.emprestimos - b.emprestimos);
+
+  return (
+    <div className="home">
+      <Container className="containerPrincipal">
+        <h1>Livros com menos empréstimos</h1>
+        {emprestimos === null ? (
+          <Loader />
+        ) : (
+          <Carousel>
+            {livrosOrdenados.map((livro) => (
+              <Carousel.Item key={livro.id}>
+                <h3>{livro.titulo}</h3>
+                <p>Quantidade de empréstimos: {livro.emprestimos}</p>
+                <img src={livro.urlCapa} alt={livro.titulo} className={"capaLivros"}/>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        )}
+      </Container>
+    </div>
+  );
 }
